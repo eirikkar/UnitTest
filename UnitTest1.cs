deee4321acb9fe7184d376d7cfaa6d99de70ec1e
eirikkar <eirikkar@proton.me>
2025-01-06 14:55:25 +0100

    add string array test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,5 +14,14 @@ public class UnitTest1
         Assert.Equal(5, Add(2, 2));
     }
 
+    [Fact]
+    public void TestStringArray()
+    {
+        string[] expected = new string[255];
+        Assert.Equal(StringArray(expected), expected);
+    }
+
     int Add(int x, int y) => x + y;
+
+    string[] StringArray(string[] arr) => arr;
 }
